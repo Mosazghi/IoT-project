0689cb6244936dbc6299dd44a32ea7bcd0861b0b
JackWardLT <larsntnu@gmail.com>
2023-10-11 22:43:06 +0200

    New Look for Sign UP

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -39,6 +39,7 @@ const LoginForm = () => {
     console.log({ username, password });
     return (
         <form onSubmit={(e) => handleSubmit(e)}>
+            {/*
             <div className="navn-wrapper">
                 <label htmlFor="navn">Navn:</label>
                 <input
@@ -68,6 +69,46 @@ const LoginForm = () => {
             >
                 Submit form
             </button>
+            */}
+            <div className="flex h-screen">
+                <div className="m-auto">
+                    <div>
+                        <div className="flex justify-center">
+                            <p className="font-bold text-5xl">SIGN UP</p>
+                        </div>
+                        <div>
+                            <p className="text-black mt-10">Username</p>
+                        </div>
+                        <input className="border-2 w-96 h-10 rounded-2xl shadow-inner pl-2 mt-2">
+                        </input>
+
+                        <div>
+                            <p className="text-black mt-3">Password</p>
+                        </div>
+
+                        <input className="border-2 w-96 h-10 rounded-2xl shadow-inner  pl-2 mt-2">
+                        </input>
+                    </div>
+                    <div className="relative bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 w-24 h-10 rounded-xl mt-2">
+                        <div className="absolute inset-1 bg-white rounded-xl">
+                            <div className="flex items-center">
+                                <input type="checkbox" id="ch1" name="ch1" class="ml-2"></input>
+                                <label for="ch1" class="pl-2 pb-2 font-bold flex self-center">ADMIN</label>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <div className=" flex justify-center">
+                        <button
+                            className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-white w-20 h-10 rounded-3xl shadow-xl"
+                        >SIGN UP
+                        </button>
+                    </div>
+
+                </div>
+            </div>
         </form>
     );
 };

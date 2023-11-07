650d21a002481ee82973b210fd602d1eec77d5f8
JackWardLT <larsntnu@gmail.com>
2023-11-07 09:53:58 +0100

    lol

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -48,10 +48,51 @@ const Form = ({ path }) => {
         <form onSubmit={(e) => handleSubmit(e)}>
             <div className="flex">
                 <div className="m-auto">
-                    <div>
-                        <div className="flex justify-center">
-                            <p className="font-bold text-5xl"> {checkMark ? "SIGN UP" : "LOGIN"}</p>
+                    <div className=" bg-white w-96 h-80 rounded-3xl shadow-md relative ">
+                        <div className="flex grid row-auto">
+                            <div className="grid row-2 flex justify-center">
+                                <div className="flex justify-center">
+                                    <div className="absolute -top-12">
+                                        <h1 className="font-bold text-7xl text-shadow-lg text-cyan-500"> {checkMark ? "SIGN UP" : "LOGIN"}</h1>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="text-black mt-10">Username</p>
+                                </div>
+                                <input
+                                    className="border-2 w-72 h-10 rounded-2xl shadow-inner pl-2 mt-2"
+                                    value={username}
+                                    name="username"
+                                    type="text"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                ></input>
+                            </div>
+                            <div className="grid row-2 flex justify-center">
+                                <div>
+                                    <p className="text-black mt-3">Password</p>
+                                </div>
+                                <input
+                                    className="border-2 w-72 h-10 rounded-2xl shadow-inner  pl-2 mt-2"
+                                    value={password}
+                                    name="password"
+                                    type="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                ></input>
+                            </div>
                         </div>
+<<<<<<< HEAD
+                        {checkMark && <RegisterCheckmark isAdmin={admin} setAdmin={setAdmin} />}
+                        <div className="mt-2 flex items-center gap-3 justify-center">
+                            <button className="< bg-cyan-500 shadow-lg shadow-cyan-500/50 hover:bg-cyan-400 text-white w-20 h-10 rounded-2xl ">
+                                {checkMark ? "REGISTER" : "LOGIN"}
+                            </button>
+                            <Link to={path === "login" ? "/register" : "/"} className="underline">
+                                {path === "login" ? "New user?" : "Already a user?"}
+                            </Link>
+                        </div>
+
+                        {error && <p className="text-red-500 text-center mt-2">{error}</p>}
+=======
                         <div>
                             <p className="text-black mt-10">Username</p>
                         </div>
@@ -61,6 +102,7 @@ const Form = ({ path }) => {
                             name="username"
                             type="text"
                             onChange={(e) => setUsername(e.target.value)}
+                            required
                         ></input>
 
                         <div>
@@ -73,19 +115,9 @@ const Form = ({ path }) => {
                             name="password"
                             type="password"
                             onChange={(e) => setPassword(e.target.value)}
+                            required
                         ></input>
-                    </div>
-                    {checkMark && <RegisterCheckmark isAdmin={admin} setAdmin={setAdmin} />}
-                    <div className="mt-2 flex items-center gap-3 justify-center">
-                        <button className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-white w-20 h-10 rounded-3xl  shadow-lg shadow-orange-300/50">
-                            {checkMark ? "REGISTER" : "LOGIN"}
-                        </button>
-                        <Link to={path === "login" ? "/register" : "/"} className="underline">
-                            {path === "login" ? "New user?" : "Already a user?"}
-                        </Link>
-                    </div>
-
-                        {error && <p className="text-red-500 text-center mt-2">{error}</p>}
+>>>>>>> d731ecb79b95c4df9d146134e33ddf300823a344
                     </div>
                     </div>
             </div>

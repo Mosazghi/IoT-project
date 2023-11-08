5994ea714bb6727835bfa0880b8282c9a8d32450
abdi <abdiraay@ntnu.no>
2023-11-08 13:01:57 +0100

    added edge computing

--- hardware/IOTtest/secrets.h
+++ hardware/IOTtest/secrets.h
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-
-#define SSID "abdi"
-#define PASSWORD "IOT12345"
-// #define SSID "mosa"
-// #define PASSWORD "mosazghi123"
-=======
 // #define SSID "abdi"
 // #define PASSWORD "IOT12345"
 #define SSID "mosa"
 #define PASSWORD "mosazghi123"
->>>>>>> Diddy
